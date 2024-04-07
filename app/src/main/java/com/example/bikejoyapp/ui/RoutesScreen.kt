86347f2e021bcf80e0df018f32f963c3a8bdc363
adria.barragan <adria.barragan@estudiantat.upc.edu>
2024-04-07 12:17:35 +0200

    Filtros distancia, duracion, ubicacion inicial en VM

--- app/src/main/java/com/example/bikejoyapp/ui/RoutesScreen.kt
+++ app/src/main/java/com/example/bikejoyapp/ui/RoutesScreen.kt
@@ -1,5 +1,6 @@
 package com.example.bikejoyapp.ui
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,6 +16,7 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.Button
@@ -28,19 +30,26 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Slider
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextFieldDefaults
 import com.example.bikejoyapp.data.MyAppRoute
 import com.example.bikejoyapp.viewmodel.MainViewModel
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
@@ -50,8 +59,6 @@ import androidx.compose.ui.text.input.KeyboardType
 fun RoutesScreen(rutesviewModel: RoutesViewModel,mainViewModel: MainViewModel) {
     val routes by rutesviewModel.routes.observeAsState(initial = emptyList())
     val searchQuery by rutesviewModel.searchQuery.observeAsState("")
-    val activeFilters by rutesviewModel.activeFilters
-    val possibleFilters = rutesviewModel.possibleFilters
 
     Column(modifier = Modifier
         .fillMaxSize()
@@ -61,15 +68,19 @@ fun RoutesScreen(rutesviewModel: RoutesViewModel,mainViewModel: MainViewModel) {
             onSearchQueryChanged = { query -> rutesviewModel.onSearchQueryChanged(query) },
             onPerformSearch = { rutesviewModel.performSearch() }
         )
-        FilterForm(possibleFilters = possibleFilters,
-            activeFilters = activeFilters,
-            onFilterChanged = { filter, isSelected ->
-                rutesviewModel.onFilterChanged(filter, isSelected)
-            },
-            onApplyFilters = {
+        FilterForm(
+            durationRange = rutesviewModel.durationRange,
+            initialDuration = rutesviewModel.durationFilter.value ?: 0f,
+            distanceRange = rutesviewModel.distanceRange,
+            initialDistance = rutesviewModel.distanceFilter.value ?: 0f,
+            initialSelectedLocation = rutesviewModel.startLocationFilter.value ?: "Cualquier zona",
+            startLocations = rutesviewModel.startLocations,
+            onApplyFilters = { duration, distance, location ->
+                rutesviewModel.onDurationChange(duration)
+                rutesviewModel.onDistanceChange(distance)
+                rutesviewModel.onStartLocationChange(location)
                 rutesviewModel.performSearchWithFilters()
             }
-
         )
         RoutesList(modifier = Modifier.weight(1f), routes = routes)
 
@@ -110,64 +121,101 @@ fun SearchBar(searchQuery: String,
 
 @Composable
 fun FilterForm(
-    possibleFilters: List<String>,
-    activeFilters: List<String>,
-    onFilterChanged: (String, Boolean) -> Unit,
-    onApplyFilters: () -> Unit
+    durationRange: ClosedFloatingPointRange<Float>,
+    initialDuration: Float,
+    distanceRange: ClosedFloatingPointRange<Float>,
+    initialDistance: Float,
+    initialSelectedLocation: String,
+    startLocations: List<String>,
+    onApplyFilters: (Float, Float, String) -> Unit
 ) {
+    var tempDuration by remember { mutableStateOf(initialDuration) }
+    var tempDistance by remember { mutableStateOf(initialDistance) }
+    var tempSelectedLocation by remember { mutableStateOf(initialSelectedLocation) }
     var showFiltersDialog by remember { mutableStateOf(false) }
 
-    Column {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween // Esto coloca los elementos al principio y al final del Row
+    ) {
         TextButton(onClick = { showFiltersDialog = true }) {
             Text("Mostrar filtros")
         }
+        Button(
+            onClick = {
+                onApplyFilters(tempDuration, tempDistance, tempSelectedLocation)
+            }
+        ) {
+            Text("Aplicar filtros")
+        }
+    }
 
-        if (showFiltersDialog) {
-            // Esta es una alternativa al menú desplegable utilizando un AlertDialog
-            AlertDialog(
-                onDismissRequest = { showFiltersDialog = false },
-                title = { Text("Filtros") },
-                text = {
-                    // Lista de todos los filtros posibles con un Checkbox
-                    Column {
-                        possibleFilters.forEach { filter ->
-                            val isChecked = activeFilters.contains(filter)
+    if (showFiltersDialog) {
+        AlertDialog(
+            onDismissRequest = { showFiltersDialog = false },
+            title = { Text("Filtros") },
+            text = {
+                Column {
+                    Text("Duración: ${tempDuration.toInt()}h")
+                    Slider(
+                        value = tempDuration,
+                        onValueChange = { tempDuration = it },
+                        valueRange = durationRange,
+                        steps = 5
+                    )
+                    Text("Distancia: ${tempDistance.toInt()}km")
+                    Slider(
+                        value = tempDistance,
+                        onValueChange = { tempDistance = it },
+                        valueRange = distanceRange,
+                        steps = 9
+                    )
+                    Text("Lugar de inicio:")
+                    LazyColumn {
+                        items(startLocations) { location ->
                             Row(
                                 verticalAlignment = Alignment.CenterVertically,
-                                modifier = Modifier.fillMaxWidth().padding(8.dp)
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .clickable { tempSelectedLocation = location }
+                                    .padding(8.dp)
                             ) {
-                                Checkbox(
-                                    checked = isChecked,
-                                    onCheckedChange = { isSelected ->
-                                        onFilterChanged(filter, isSelected)
-                                    }
+                                if (location == tempSelectedLocation) {
+                                    Icon(
+                                        imageVector = Icons.Default.Check,
+                                        contentDescription = "Seleccionado",
+                                        tint = Color.Green // Ajusta el color del ícono aquí
+                                    )
+                                }
+                                Spacer(modifier = Modifier.width(8.dp)) // Espacio entre el icono y el texto
+                                Text(
+                                    text = location,
+                                    color = if (location == tempSelectedLocation) Color.Green else Color.Black, // Cambia el color del texto
+                                    modifier = Modifier.weight(1f)
                                 )
-                                Text(text = filter)
                             }
                         }
                     }
-                },
-                confirmButton = {
-                    Button(
-                        onClick = {
-                            onApplyFilters()
-                            showFiltersDialog = false
-                        }
-                    ) {
-                        Text("Aplicar")
-                    }
-                },
-                dismissButton = {
-                    TextButton(
-                        onClick = { showFiltersDialog = false }
-                    ) {
-                        Text("Cancelar")
-                    }
                 }
-            )
-        }
+            },
+            confirmButton = {
+                TextButton(
+                    onClick = { showFiltersDialog = false }
+                ) { Text("Aceptar") }
+            },
+            dismissButton = {
+                TextButton(
+                    onClick = { showFiltersDialog = false }
+                ) { Text("Cancelar") }
+            }
+        )
     }
 }
+
+
 
 
 
--- app/src/main/java/com/example/bikejoyapp/viewmodel/RoutesViewModel.kt
+++ app/src/main/java/com/example/bikejoyapp/viewmodel/RoutesViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.bikejoyapp.viewmodel
 
+import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
@@ -10,6 +11,12 @@ import com.example.bikejoyapp.R
 import com.example.bikejoyapp.data.MyAppRoute
 import com.example.bikejoyapp.data.Route
 import com.example.bikejoyapp.ui.GravarRutaScreen
+import retrofit2.http.GET
+import retrofit2.http.Query
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 
 class RoutesViewModel : ViewModel() {
     // LiveData que contiene la lista de rutas
@@ -19,12 +26,56 @@ class RoutesViewModel : ViewModel() {
     private val _searchQuery = MutableLiveData<String>()
     val searchQuery: LiveData<String> = _searchQuery
 
-    private val _possibleFilters = MutableLiveData<List<String>>()
-    val possibleFilters = listOf("Corta", "Media", "Larga")
+    val durationRange = 0f..6f
+
+    private val _durationFilter = MutableLiveData<Float>()
+    val durationFilter: LiveData<Float> = _durationFilter
+
+    val distanceRange = 0f..10f
+
+    private val _distanceFilter = MutableLiveData<Float>()
+    val distanceFilter: LiveData<Float> = _distanceFilter
+
+    val startLocations = listOf("Cualquier zona",
+        "Barri Gotic",
+        "El Poble Sec",
+        "El Born",
+        "El Clot",
+        "El Poblenou",
+        "El Putxet",
+        "El Raval",
+        "El Tibidabo",
+        "El Vall d'Hebron",
+        "Horta",
+        "La Barceloneta",
+        "La Sagrada Familia",
+        "Les Corts",
+        "Sant Andreu",
+        "Sant Antoni",
+        "Sant Gervasi",
+        "Sant Marti",
+        "Sants",
+        "Sarria")
+
+    private val _startLocationFilter = MutableLiveData<String>()
+    val startLocationFilter: LiveData<String> = _startLocationFilter
 
 
-    private val _activeFilters = mutableStateOf<List<String>>(emptyList())
-    val activeFilters: MutableState<List<String>> = _activeFilters
+
+    fun onDurationChange(value: Float) {
+        _durationFilter.value = value
+        Log.d("Filters", "Duración cambiada a: $value")
+    }
+
+    fun onDistanceChange(value: Float) {
+        _distanceFilter.value = value
+        Log.d("Filters", "Distancia cambiada a: $value")
+    }
+
+    fun onStartLocationChange(location: String) {
+        _startLocationFilter.value = location
+        Log.d("Filters", "Ubicación de inicio cambiada a: $location")
+    }
 
 
     init {
@@ -46,24 +97,25 @@ class RoutesViewModel : ViewModel() {
         _searchQuery.value = query
     }
 
-    fun onFilterChanged(filter: String, isSelected: Boolean) {
-        val currentFilters = activeFilters.value.toMutableList()
-        if (isSelected && !currentFilters.contains(filter)) {
-            currentFilters.add(filter)
-        } else if (!isSelected) {
-            currentFilters.remove(filter)
-        }
-        activeFilters.value = currentFilters
-    }
 
     // Función para realizar la búsqueda con los filtros activos.
     fun performSearchWithFilters() {
-        // Aquí podrías llamar a la API de tu backend pasando los filtros activos.
-        // Esta sería una llamada de red similar a la implementación de la función performSearch().
+        // Aquí deberías implementar la lógica de búsqueda con los filtros activos
     }
     fun performSearch() {
         // Aquí deberías implementar la lógica de búsqueda
     }
+
+    interface ApiService {
+        // Asume que ya tienes otros endpoints aquí
+        @GET("searchRoutes")
+        suspend fun searchRoutes(
+            @Query("duration") duration: Float,
+            @Query("distance") distance: Float,
+            @Query("startLocation") startLocation: String
+        ): Response<List<Route>>
+    }
+
 
 
 
