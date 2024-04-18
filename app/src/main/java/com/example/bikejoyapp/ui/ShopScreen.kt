968697406961bce6e05cb801c0d929b1e29a6988
nil.del.pozo <nil.del.pozo@estudiantat.upc.edu>
2024-04-18 14:14:41 +0200

    primera versió ShopScreen

--- app/src/main/java/com/example/bikejoyapp/ui/ShopScreen.kt
+++ app/src/main/java/com/example/bikejoyapp/ui/ShopScreen.kt
@@ -1,25 +1,114 @@
 package com.example.bikejoyapp.ui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.bikejoyapp.R
+import com.example.bikejoyapp.data.Item
 import com.example.bikejoyapp.viewmodel.ShopViewModel
 
 @Composable
 fun ShopScreen(shopViewModel: ShopViewModel) {
-    val items = shopViewModel.items.value
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+    val items = shopViewModel.items.value ?: emptyList()
+    Column(modifier = Modifier.fillMaxSize()) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalArrangement = Arrangement.End,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text("1000")
+            Spacer(modifier = Modifier.width(8.dp))
+            Icon(
+                painter = painterResource(id = R.drawable.dollar_minimalistic_svgrepo_com),
+                contentDescription = "Localized description",
+                modifier = Modifier.size(32.dp)
+            )
+        }
+        //Spacer(modifier = Modifier.height(8.dp))
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            contentPadding = PaddingValues(8.dp),
+            modifier = Modifier.fillMaxSize()
+        ) {
+            items(items.size) { index ->
+                ItemCard(index = index, items = items)
+            }
+        }
+    }
+}
+
+@Composable
+fun ItemCard(index: Int, items: List<Item>) {
+    val item = items[index]
+    Card(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth(),
+        elevation = CardDefaults.cardElevation(6.dp),
     ) {
-        Text(text = "Shop")
-        Spacer(modifier = Modifier.weight(1f))
-        Text(text = items?.size.toString())
+        Box(modifier = Modifier.height(150.dp)) {
+            Image(
+                painter = painterResource(R.drawable.item_1_default),
+                contentDescription = "Item Image",
+                contentScale = ContentScale.Crop
+            )
+            Box (modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    Brush.verticalGradient(
+                        colors = listOf(
+                            Color.Transparent,
+                            Color.Black
+                        ),
+                        startY = 200f
+                    )
+                )
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(12.dp),
+                contentAlignment = Alignment.BottomCenter
+            ) {
+                Text(
+                    text = "Price: ${item.game_currency_price}",
+                    color = Color.White,
+                    fontSize = 16.sp,
+                )
+            }
+        }
     }
 }
