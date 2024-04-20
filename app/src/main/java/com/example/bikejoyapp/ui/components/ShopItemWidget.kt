c5bdc03342184cabfad80d915480306fdd7c6cc0
nil.del.pozo <nil.del.pozo@estudiantat.upc.edu>
2024-04-20 17:55:28 +0200

    botón de comprar sin acción

--- app/src/main/java/com/example/bikejoyapp/ui/components/ShopItemWidget.kt
+++ app/src/main/java/com/example/bikejoyapp/ui/components/ShopItemWidget.kt
@@ -1,19 +1,31 @@
 package com.example.bikejoyapp.ui.components
 
+import android.graphics.Color.rgb
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.livedata.observeAsState
@@ -21,6 +33,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -84,5 +97,48 @@ fun ShopItemWidget(
             modifier = Modifier.padding(horizontal = 16.dp),
             textAlign = TextAlign.Center
         )
+        Spacer(modifier = Modifier.weight(1f)) // This will push the button to the bottom
+        Column(
+            modifier = Modifier
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            OutlinedButton(
+                elevation = ButtonDefaults.buttonElevation(
+                    defaultElevation = 2.dp,
+                    pressedElevation = 8.dp,
+                    disabledElevation = 0.dp,
+                    hoveredElevation = 4.dp,
+                    focusedElevation = 4.dp
+                ),
+                onClick = {
+
+                },
+                modifier = Modifier
+                    .padding(horizontal = 24.dp, vertical = 16.dp)
+                    .clip(RoundedCornerShape(50)),
+                    colors = ButtonDefaults.outlinedButtonColors(
+                        containerColor = Color(rgb(235, 235, 235)),
+                        contentColor = Color.Black
+                    ),
+                contentPadding = PaddingValues(
+                    horizontal = 24.dp,
+                    vertical = 10.dp
+                ),
+                border = BorderStroke(2.dp, Color.Black)
+            ) {
+                Text(
+                    text = item?.game_currency_price.toString(),
+                    style = MaterialTheme.typography.labelMedium,
+                )
+                Spacer(modifier = Modifier.size(4.dp))
+                Icon(
+                    painter = painterResource(id = R.drawable.dollar_minimalistic_svgrepo_com),
+                    contentDescription = "coin",
+                    modifier = Modifier.size(32.dp),
+                    tint = Color(0xFFD4AF37)
+                )
+            }
+        }
     }
 }
